086476665a7e93d82a431e4621631cc07c784b0c
Lan714 <87465185+Lan714@users.noreply.github.com>
2021-10-16 16:34:55 -0700

    join path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(require('./routes'))
 
 app.get('*', (req, res) => {
-  res.sendFile(join(__dirname, './public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.listen(process.env.PORT || 3000)
